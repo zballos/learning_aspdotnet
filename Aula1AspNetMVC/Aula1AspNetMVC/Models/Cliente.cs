a274b1dd6d8557e6c0bcd5e1d3a79616fd270dfe
Edson Zeballos <09edson@gmail.com>
2017-09-03 14:56:01 -0400

    Iniciando com asp net mvc 5

---
+++ Aula1AspNetMVC/Aula1AspNetMVC/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula1AspNetMVC.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
