9c8fbd633a28985fbe7f1adc6656e01a1a994cc2
Edson Zeballos <09edson@gmail.com>
2017-09-11 22:15:33 -0400

    Criando a infraestrutura, incluindo EntityFramework, Configurando Context e Mapeando as classes do banco de dados inicial

---
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Infra.Data/Context/MvcDotNetContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using EZ.MvcDotNet.Domain.Entities;
+
+namespace EZ.MvcDotNet.Infra.Data.Context
+{
+    class MvcDotNetContext : DbContext
+    {
+        public MvcDotNetContext()
+            : base("MvcDotNetContext")
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
