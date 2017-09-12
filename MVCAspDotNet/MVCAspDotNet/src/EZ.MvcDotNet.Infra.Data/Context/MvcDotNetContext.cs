09ea821db2edcb1a878500292cbf74bf2e545e8a
Edson Zeballos <09edson@gmail.com>
2017-09-11 22:27:00 -0400

    Configuração generica do OnModelCreating

--- MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Infra.Data/Context/MvcDotNetContext.cs
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Infra.Data/Context/MvcDotNetContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using EZ.MvcDotNet.Domain.Entities;
 
 namespace EZ.MvcDotNet.Infra.Data.Context
@@ -16,6 +17,21 @@ namespace EZ.MvcDotNet.Infra.Data.Context
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            // General Custom Context Properties
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
             base.OnModelCreating(modelBuilder);
         }
 
