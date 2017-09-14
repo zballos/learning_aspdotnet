4620715afbc0dc3407be557b996f9c78ca3e7597
Edson Zeballos <09edson@gmail.com>
2017-09-13 23:03:22 -0400

    Configuração e profiles do AutoMapper

---
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EZ.MvcDotNet.Application.ViewModels;
+using EZ.MvcDotNet.Domain.Entities;
+
+namespace EZ.MvcDotNet.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ClienteViewModel, Cliente>();
+            CreateMap<ClienteEnderecoViewModel, Cliente>();
+            CreateMap<ClienteEnderecoViewModel, Endereco>();
+            CreateMap<EnderecoViewModel, Endereco>();
+        }
+    }
+}
