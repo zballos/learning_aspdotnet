3932db48071dcfc3ab47eaeedf7fd925cbdf2641
Edson Zeballos <09edson@gmail.com>
2017-09-13 23:05:32 -0400

    Gerando uma demo simples com clientes e enderecos

---
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.UI.MVC/Controllers/ClientesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EZ.MvcDotNet.Application;
+using EZ.MvcDotNet.Application.ViewModels;
+using EZ.MvcDotNet.UI.MVC.Models;
+
+namespace EZ.MvcDotNet.UI.MVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ClienteAppService _clienteAppService = new ClienteAppService();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(_clienteAppService.ObterTodos());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClienteViewModel clienteViewModel = _clienteAppService.ObterPorId(id.Value);
+            if (clienteViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clienteViewModel);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClienteEnderecoViewModel clienteEnderecoViewModel) // [Bind(Include = "ClienteId,Nome,Email,CPF,DataNascimento,DataCadastro,Ativo")]
+        {
+            if (ModelState.IsValid)
+            {
+                _clienteAppService.Adicionar(clienteEnderecoViewModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(clienteEnderecoViewModel);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var clienteViewModel = _clienteAppService.ObterPorId(id.Value);
+            if (clienteViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clienteViewModel);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClienteViewModel clienteViewModel) // [Bind(Include = "ClienteId,Nome,Email,CPF,DataNascimento,DataCadastro,Ativo")]
+        {
+            if (ModelState.IsValid)
+            {
+                _clienteAppService.Atualizar(clienteViewModel);
+                return RedirectToAction("Index");
+            }
+            return View(clienteViewModel);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var clienteViewModel = _clienteAppService.ObterPorId(id.Value);
+            if (clienteViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clienteViewModel);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            _clienteAppService.Remover(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _clienteAppService.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
