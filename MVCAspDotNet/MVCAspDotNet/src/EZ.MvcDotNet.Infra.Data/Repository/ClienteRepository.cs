6d1752de5a49978e3a41779ed23c68e2e2b4d407
Edson Zeballos <09edson@gmail.com>
2017-09-19 22:28:52 -0400

    Incluindo Dapper e configurando uma consulta otimizada

--- MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Infra.Data/Repository/ClienteRepository.cs
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Infra.Data/Repository/ClienteRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using Dapper;
 using EZ.MvcDotNet.Domain.Entities;
 using EZ.MvcDotNet.Domain.Interfaces.Repository;
 
@@ -14,6 +16,35 @@ namespace EZ.MvcDotNet.Infra.Data.Repository
         public Cliente ObterPorEmail(string email)
         {
             return Buscar(c => c.Email == email).FirstOrDefault();
+        }
+
+
+        /// Reescrevendo filtro com Dapper 
+        /// 
+        /// O Dapper é indicado para otimizar as consultas a banco
+        public override Cliente ObterPorId(Guid id)
+        {
+            var sql = @"SELECT * FROM Clientes c " +
+                      "INNER JOIN Enderecos e ON c.ClienteId = e.ClienteId " +
+                      "WHERE c.ClienteId = @sid ";
+
+            using (var cn = Db.Database.Connection)
+            {
+                cn.Open();
+                // Consulta Simples
+                // var cliente = cn.Query<Cliente>(sql, new {sid = id});
+
+                // Consulta Completa
+                var cliente = cn.Query<Cliente, Endereco, Cliente>(sql,
+                    (c, e) =>
+                    {
+                        c.Enderecos.Add(e);
+                        return c;
+                    }, new { sid = id}, splitOn: "ClienteId, EnderecoId");
+
+
+                return cliente.FirstOrDefault();
+            }
         }
     }
 }
