68a823cf7d76b5e844785c17a24a2b0348a51dca
Edson Zeballos <09edson@gmail.com>
2017-09-29 00:18:35 -0400

    Specifications

---
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Domain/Specification/Clientes/ClienteDevePossuirCpfValidoSpecification.cs
@@ -0,0 +1,14 @@
+﻿using DomainValidation.Interfaces.Specification;
+using EZ.MvcDotNet.Domain.Entities;
+using EZ.MvcDotNet.Domain.Validation.Documentos;
+
+namespace EZ.MvcDotNet.Domain.Specification.Clientes
+{
+    public class ClienteDevePossuirCpfValidoSpecification : ISpecification<Cliente>
+    {
+        public bool IsSatisfiedBy(Cliente cliente)
+        {
+            return CPFValidation.Validar(cliente.CPF);
+        }
+    }
+}
---
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Domain/Specification/Clientes/ClienteDeveSerMaiorDeIdadeSpecification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DomainValidation.Interfaces.Specification;
+using EZ.MvcDotNet.Domain.Entities;
+
+namespace EZ.MvcDotNet.Domain.Specification.Clientes
+{
+    public class ClienteDeveSerMaiorDeIdadeSpecification : ISpecification<Cliente>
+    {
+        public bool IsSatisfiedBy(Cliente cliente)
+        {
+            // superficial
+            return DateTime.Now.Year - cliente.DataNascimento.Year >= 18;
+        }
+    }
+}
