b99493e068eaeeedc5768475b735a452bb3e7d5c
Edson Zeballos <09edson@gmail.com>
2017-09-29 00:19:20 -0400

    Validations

---
+++ MVCAspDotNet/MVCAspDotNet/src/EZ.MvcDotNet.Domain/Validation/Clientes/ClienteEstaConsistenteValidation.cs
@@ -0,0 +1,22 @@
+﻿using DomainValidation.Validation;
+using EZ.MvcDotNet.Domain.Entities;
+using EZ.MvcDotNet.Domain.Specification.Clientes;
+
+namespace EZ.MvcDotNet.Domain.Validation.Clientes
+{
+    public class ClienteEstaConsistenteValidation : Validator<Cliente>
+    {
+        public ClienteEstaConsistenteValidation()
+        {
+            var clienteCPF = new ClienteDevePossuirCpfValidoSpecification();
+            var clienteEmail = new ClienteDevePossuirEmailValidoSpecification();
+            var clienteMaiorIdade = new ClienteDeveSerMaiorDeIdadeSpecification();
+
+            base.Add("clienteCPF", new Rule<Cliente>(clienteCPF, "Cliente informou um CPF inválido!"));
+            base.Add("clienteEmail", new Rule<Cliente>(clienteEmail, "Cliente informou um Email inválido!"));
+            base.Add("clienteMaiorIdade", new Rule<Cliente>(clienteMaiorIdade, "Cliente não tem maioridade para cadastro!"));
+        }
+
+        
+    }
+}
